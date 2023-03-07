21d3fd25eb4b8f95aba2215b6aa7b29ca86b4b0c
Phoques <109713674+Phoques@users.noreply.github.com>
2023-03-07 13:57:14 +1100

    Added Car
    Need to fix car wheels etc

--- Assets/CarControl.cs
+++ Assets/CarControl.cs
@@ -6,31 +6,74 @@ public class CarControl : MonoBehaviour
 {
 
     HingeJoint[] joints;
+    public float wheelTurn;
+
+    public GameObject frontWheelLeft;
 
 
     private void Start()
     {
         joints = GetComponentsInChildren<HingeJoint>();
+        
+        
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.W))
         {
-            foreach(HingeJoint joint in joints)
+            foreach (HingeJoint joint in joints)
+            {
+                JointMotor motor = joint.motor;
+                motor.targetVelocity -= 200 * Time.deltaTime;
+                joint.motor = motor;
+
+
+            }
+
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            foreach (HingeJoint joint in joints)
             {
                 JointMotor motor = joint.motor;
                 motor.targetVelocity += 200 * Time.deltaTime;
                 joint.motor = motor;
 
-               
-            }
 
-           
+            }
         }
+
+        //Good for rotating an object that doesnt use gravity.
+        /*if (Input.GetKey(KeyCode.D))
+        {
+            //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+            m_EulerAngleVelocity = new Vector3(0, 500, 0);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //Set the angular velocity of the Rigidbody (rotating around the Y axis, 100 deg/sec)
+            m_EulerAngleVelocity = new Vector3(0, -500, 0);
+            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.fixedDeltaTime);
+            rb.MoveRotation(rb.rotation * deltaRotation);
+        }*/
+
+        
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            frontWheelLeft.transform.Rotate(50, 0, 0, Space.Self);
+        }
+
     }
 
-
+    /*public void AddTorque(float x, float y, float z, ForceMode mode = ForceMode.Force);*/
 
 
 }
